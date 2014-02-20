503a274d7474fe78c7370bcf8bf7f87e09b30847
Jim White <james.paul.white@gmail.com>
2014-02-19 23:55:24 -0800

    SecuredScript should be tidy and put things back the way it found them.

--- src/org/ifcx/gondor/SecuredScript.java
+++ src/org/ifcx/gondor/SecuredScript.java
@@ -8,45 +8,50 @@ abstract public class SecuredScript extends Script {
     abstract protected Object runScript();
 
     public Object run() {
-        System.setSecurityManager(new NullSecurityManager() {
-            @Override
-            public void checkRead(FileDescriptor fd) {
-                System.err.println("read fd : " + nameForFileDescriptor(fd));
-                super.checkRead(fd);
-            }
+        SecurityManager oldSM = System.getSecurityManager();
+        try {
+            System.setSecurityManager(new NullSecurityManager() {
+                @Override
+                public void checkRead(FileDescriptor fd) {
+                    System.err.println("read fd : " + nameForFileDescriptor(fd));
+                    super.checkRead(fd);
+                }
 
-            @Override
-            public void checkRead(String file) {
-                System.err.println("read fs : " + file);
-                super.checkRead(file);
-            }
+                @Override
+                public void checkRead(String file) {
+                    System.err.println("read fs : " + file);
+                    super.checkRead(file);
+                }
 
-            @Override
-            public void checkRead(String file, Object context) {
-                System.err.println("read fs in ctx : " + file + " ctx " + context);
-                super.checkRead(file, context);
-            }
+                @Override
+                public void checkRead(String file, Object context) {
+                    System.err.println("read fs in ctx : " + file + " ctx " + context);
+                    super.checkRead(file, context);
+                }
 
-            @Override
-            public void checkWrite(FileDescriptor fd) {
-                System.err.println("write fd : " + nameForFileDescriptor(fd));
-                super.checkWrite(fd);
-            }
+                @Override
+                public void checkWrite(FileDescriptor fd) {
+                    System.err.println("write fd : " + nameForFileDescriptor(fd));
+                    super.checkWrite(fd);
+                }
 
-            private String nameForFileDescriptor(FileDescriptor fd) {
-                if (FileDescriptor.out == fd) return "out";
-                if (FileDescriptor.err == fd) return "err";
-                if (FileDescriptor.in == fd) return "in";
-                return "unk";
-            }
+                private String nameForFileDescriptor(FileDescriptor fd) {
+                    if (FileDescriptor.out == fd) return "out";
+                    if (FileDescriptor.err == fd) return "err";
+                    if (FileDescriptor.in == fd) return "in";
+                    return "unk";
+                }
 
-            @Override
-            public void checkWrite(String file) {
-                System.err.println("write fs : " + file);
-                super.checkWrite(file);
-            }
-        });
+                @Override
+                public void checkWrite(String file) {
+                    System.err.println("write fs : " + file);
+                    super.checkWrite(file);
+                }
+            });
 
-        return runScript();
+            return runScript();
+        } finally {
+            System.setSecurityManager(oldSM);
+        }
     }
 }
