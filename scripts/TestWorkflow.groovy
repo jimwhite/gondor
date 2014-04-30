7e64407d2fc7bbe30e4cff1d58acca0c7fe4f539
Jim White <james.paul.white@gmail.com>
2014-04-30 14:06:39 -0500

    Working tiny workflow example.

--- scripts/TestWorkflow.groovy
+++ scripts/TestWorkflow.groovy
@@ -1,4 +1,11 @@
+import org.ifcx.gondor.Command
 
 @groovy.transform.BaseScript org.ifcx.gondor.WorkflowScript thisScript
 
-ls('.')
+def ls = command(path:'/bin/ls') { infile 'path' }
+
+def grep = command(path:'/usr/bin/grep') { arg 'pat', { it }, Command.REQUIRED }
+
+// ls('.')
+(ls(path:new File('.')) | grep(pat:/Work/)) >> new File('grep_out.txt')
+
