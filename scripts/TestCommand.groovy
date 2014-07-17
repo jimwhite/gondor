e1b5f2b3c4fbac4738e9f69a0dba46697edd795e
Jim White <james.paul.white@gmail.com>
2014-07-17 12:37:59 -0700

    Demonstrate parameter converter closures.

--- scripts/TestCommand.groovy
+++ scripts/TestCommand.groovy
@@ -1,20 +1,22 @@
 #!/usr/bin/env groovy
 
 import com.beust.jcommander.Parameter
+import com.beust.jcommander.converters.BaseConverter
 import groovy.transform.Field
 import groovyx.cli.JCommanderScript
-import org.ifcx.gondor.Command
+
+import java.util.regex.Pattern
 
 @groovy.transform.BaseScript JCommanderScript thisScript
 
 @Parameter(names='--path') @Field File path
 
-@Parameter(names='--pattern') @Field String pat
+@Parameter(names='--pattern', converter = { java.util.regex.Pattern.compile(it) } ) @Field pat
 
-@Parameter @Field List<File> paths
+@Parameter @Field List<File> paths = []
 
 @Parameter(names=['--help', '-h'], help=true, arity=-1) @Field String foo
 
-[path, *paths].each { File dir -> dir.listFiles().name.findAll { it =~ pattern  }.each { println it } }
+[path, *paths].each { File dir -> dir.listFiles().name.findAll { pat.matcher(it).matches() }.each { println it } }
 
 0
